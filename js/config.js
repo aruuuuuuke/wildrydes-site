65672875f227806d061a48175a6b935419afc845
aruuuuuuke <144872061+aruuuuuuke@users.noreply.github.com>
2024-11-19 09:02:51 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_jwoSXgsUw', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5tk1bv05h96el3lfdhj7347ffj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_oKqkpmkAl', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7e9hfmd998tf62h856qq73r8pt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://h8ca4r7md3.execute-api.eu-north-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
